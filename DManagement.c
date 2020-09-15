3bf373c9a2cfcb123560cd2f4b54368f6e743269
Frasier Sundra <fsundra@gmail.com>
2020-09-15 20:57:58 +0800

    Added fileexists function

--- DManagement.c
+++ DManagement.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+#include <stdbool.h>
+
+int fileexists(const char *filename) {
+    FILE *file;
+    if (file = fopen(filename, "r")) {
+        fclose(file);
+        return 1;
+    }
+    return 0;
+}
 
 int password(char pw[]) 
 {
@@ -16,17 +26,45 @@ int password(char pw[])
     
 }
 
-void WelcomeScreen(void) 
+int WelcomeScreen(void) 
 {
     system("cls");
+    int input = 0;
     printf("THIS IS YOUR DIARY MANAGEMENT SYSTEM\n");
-    printf("1. Create new Entry\n2. Edit Entry\n3. Remove Entry\n4. Exit");
+    printf("1. Create new Entry\n2. Edit Entry\n3. Remove Entry\n4. Exit\n");
+    sscanf("%d", &input);
+    getchar();
+    switch(input) {
+        case 1:
+            printf("Input not recognised")
+    }
+    
+
 }
 
-
+void Diary(void)
+{
+    int closediary = false;
+    while (closediary != true)
+    {
+        WelcomeScreen();
+        closediary = true;
+    }
+}
 
 int main(int argc, char * argv[]) {
 
+    //FILE CHECKING
+    char* filename = argv[2];
+    int exists = fileexists(argv[2]);
+    if (exists == 1) {
+        printf("Reading from file %s\n", argv[2]);
+    } else {
+        printf("File not found \n");
+        exit(EXIT_FAILURE);
+    }
+
+    //PASSWORD CHECKING
     int pwCheck = password(argv[1]);
 
     if (pwCheck == 1) {
@@ -40,11 +78,8 @@ int main(int argc, char * argv[]) {
     }
     else {exit(EXIT_FAILURE);}
 
-    WelcomeScreen();
-
-
-
-
+    //INITIALISING DIARY FUNCTION
+    Diary();
 
 
     getchar();
